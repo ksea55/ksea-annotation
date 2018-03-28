45b2d60e4c482c2271f3d0306a471b5382d6b4a1
ksea55 <wangyang198118@126.com>
2018-03-28 09:53:13 +0800

    扩展原理：      * BeanPostProcessor：bean后置处理器，bean创建对象初始化前后进行拦截工作的      *      * BeanFactoryPostProcessor：beanFactory的后置处理器；      * 		在BeanFactory标准初始化之后调用，来定制和修改BeanFactory的内容；      * 		所有的bean定义已经保存加载到beanFactory，但是bean的实例还未创建      *      *      * BeanFactoryPostProcessor原理:      * 1)、ioc容器创建对象      * 2)、invokeBeanFactoryPostProcessors(beanFactory);      * 		如何找到所有的BeanFactoryPostProcessor并执行他们的方法；      * 			1）、直接在BeanFactory中找到所有类型是BeanFactoryPostProcessor的组件，并执行他们的方法      * 			2）、在初始化创建其他组件前面执行

--- src/main/java/com/ksea/spring/chapter16/MainConfig.java
+++ src/main/java/com/ksea/spring/chapter16/MainConfig.java
@@ -8,10 +8,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MainConfig {
     /**
-     * BeanPostProcessor:bean的后置处理器，在bean初始化前后进行执行
-     * BeanFactoryPostProcessor: beanFactory后置处理器,它是在bean初始化前进行执行，具体在bean已经定义加载，但是还没被实例化
-     * 1:IOC初始化
-     * 2:invokeBeanFactoryPostProcessors(beanFactory);
+     * 扩展原理：
+     * BeanPostProcessor：bean后置处理器，bean创建对象初始化前后进行拦截工作的
+     *
+     * BeanFactoryPostProcessor：beanFactory的后置处理器；
+     * 		在BeanFactory标准初始化之后调用，来定制和修改BeanFactory的内容；
+     * 		所有的bean定义已经保存加载到beanFactory，但是bean的实例还未创建
+     *
+     *
+     * BeanFactoryPostProcessor原理:
+     * 1)、ioc容器创建对象
+     * 2)、invokeBeanFactoryPostProcessors(beanFactory);
+     * 		如何找到所有的BeanFactoryPostProcessor并执行他们的方法；
+     * 			1）、直接在BeanFactory中找到所有类型是BeanFactoryPostProcessor的组件，并执行他们的方法
+     * 			2）、在初始化创建其他组件前面执行
      * 源码注释
      * Modify the application context's internal bean factory after its standard
      * initialization. All bean definitions will have been loaded, but no beans
